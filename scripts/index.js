0a30801b05089408d73d3d9573f21f7e74cda942
renzootoya <renzo.aldair.otoya@gmail.com>
2025-06-04 17:45:05 -0400

    New updated project sPOTS

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,23 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfilemodal = document.querySelector("#edit-profile-modal");
-const editProfileClosBtn = editProfilemodal.querySelector(".modal__close-btn");
+const editProfileCloseBtn = editProfilemodal.querySelector(".modal__close-btn");
 
 editProfileBtn.addEventListener("click", function () {
   editProfilemodal.classList.add("modal_is-opened");
 });
 
-editProfileBtn.addEventListener("click", function () {
-  editProfileClosBtn.classList.remove("modal_is-opened");
+editProfileCloseBtn.addEventListener("click", function () {
+  editProfilemodal.classList.remove("modal_is-opened");
+});
+
+const newPostBtn = document.querySelector(".profile__add-btn");
+const newPostmodal = document.querySelector("#new-post-modal");
+const newPostCloseBtn = newPostmodal.querySelector(".modal__close-btn");
+
+newPostBtn.addEventListener("click", function () {
+  newPostmodal.classList.add("modal_is-opened");
+});
+
+newPostCloseBtn.addEventListener("click", function () {
+  newPostmodal.classList.remove("modal_is-opened");
 });
